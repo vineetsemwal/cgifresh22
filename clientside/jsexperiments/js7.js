556cefcca7015bf8768be8e29693c4fe57a7261a
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-18 15:30:20 +0530

    double equal,triple equal

--- clientside/jsexperiments/js7.js
+++ clientside/jsexperiments/js7.js
@@ -1,6 +1,16 @@
 /*
 double equal vs triple equal
+double equal to only compare value
+triple equal compare value + data type 
 */
 
-var val1=10;
+var value1=10;
 
+var value2="10";
+
+var doubleIsEqual= value1==value2;
+
+console.log("double equal="+doubleIsEqual);
+
+var tripleIsEqual= value1===value2;
+console.log("triple equal to="+tripleIsEqual);
