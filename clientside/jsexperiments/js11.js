1d9fcebcaab88074777eeb6d64b8cd6c9bc55631
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-18 16:07:10 +0530

    array js demo

---
+++ clientside/jsexperiments/js11.js
@@ -0,0 +1,14 @@
+/**
+ * array
+ */
+
+const array1=["one", "two", "three"];
+
+array1.push("four");
+
+for(let i=0;i<array1.length;i++){
+  let iteratedElement=  array1[i];
+  console.log("element="+iteratedElement);
+}
+
+
