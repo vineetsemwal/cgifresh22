21f7b539d3f524d88e7f955e11bd94a2c6816c27
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-18 16:02:30 +0530

    arrow function

---
+++ clientside/jsexperiments/js10.js
@@ -0,0 +1,19 @@
+/**
+ * arrow function
+ */
+
+
+var add=(num1, num2)=>{
+    var result=num1+num2;
+    return result;
+}
+
+var res1=add(3,5);
+
+console.log("add result is "+res1);
+
+var mul=(num1, num2)=> num1*num2;
+
+var res2=mul(2,5);
+
+console.log("mltiply result="+res2);
