b7a432a9087c38d1a63fca811b8f887610af64d4
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-18 16:17:18 +0530

    for each with of vs in

---
+++ clientside/jsexperiments/js12.js
@@ -0,0 +1,20 @@
+/**
+ * for each  -- of vs in
+ */
+
+const array1=["one", "two", "three"];
+
+array1.push("four");
+
+console.log("for each with of");
+
+for(let element of array1){
+ console.log("element="+element);
+
+}
+
+console.log("for each with in");
+for(let index in array1){
+  let element=array1[index];
+  console.log("index="+index +"element="+element) ;
+} 
