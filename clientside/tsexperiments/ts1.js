fbae744ea4970a3c05403e8f48b0e41a01dda85a
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-19 16:26:14 +0530

    typescript function, variable demo

---
+++ clientside/tsexperiments/ts1.js
@@ -0,0 +1,8 @@
+var userName = "ragul"; // strict typed
+console.log("username=" + userName);
+function add(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+var result = add(1, 2);
+console.log("result=" + result);
---
+++ clientside/tsexperiments/ts1.ts
@@ -0,0 +1,13 @@
+let userName:string ="ragul"; // strict typed
+
+console.log("username="+userName);
+
+
+function add(num1:number,num2:number) : number{
+    const result : number =num1+num2;
+    return result;
+}
+
+let result :number=add(1,2);
+
+console.log("result="+result);
