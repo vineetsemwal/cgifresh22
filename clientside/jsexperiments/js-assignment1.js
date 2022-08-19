fb61535b42ab925b9e1c3099bc05cb2688bea3fb
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-19 14:54:31 +0530

    js1 assignment

---
+++ clientside/jsexperiments/js-assignment1.js
@@ -0,0 +1,61 @@
+class Product {
+
+    name;
+    price;
+    constructor(name, price) {
+        this.price = price;
+        this.name = name;
+    }
+}
+
+class ProductService {
+    products = [];
+
+    addProduct(name, price) {
+        const product = new Product(name, price);
+        this.products.push(product);
+    }
+
+    findByName(name) {
+        for (const product of this.products) {
+            if (product.name === name) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    findAll() {
+        return this.products;
+    }
+
+
+}
+
+class Frontend {
+    service = new ProductService();
+
+    runUI() {
+        this.service.addProduct("samsung", 20000);
+        this.service.addProduct("moto", 15000);
+
+        const found = this.service.findByName("samsung");
+        console.log("****displaying fetched product");
+        this.display(found);
+
+        console.log("****display all products in store after fetch");
+        const products = this.service.findAll();
+        for (let product of products) {
+            this.display(product);
+        }
+    }
+
+    display(product) {
+        console.log("name=" + product.name + " price=" + product.price);
+    }
+
+}
+
+
+const frontend = new Frontend();
+frontend.runUI();
