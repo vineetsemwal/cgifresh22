dbe755a4cbb4fdb5762648fb5da607bbe5dbc44c
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-23 16:39:06 +0530

    model view usage demo

--- clientside/angular-projects/first-app/src/app/app.component.ts
+++ clientside/angular-projects/first-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Developer, Employee, Tester } from './employee';
-import { EmployeeService } from './employee.service';
+import { Developer, Tester } from './employee';
 
 @Component(
 
@@ -13,27 +12,11 @@ import { EmployeeService } from './employee.service';
 )
 
 export class AppComponent {
- service = new EmployeeService();
-  foundDeveloper: Developer =new Developer("",-1,"","");
-  foundTester: Tester =new Tester("",-1,"","");
-  error: any | undefined;
-
-
+  
+ developer1=new Developer("taran",21, "dev", "java");
+ tester1=new Tester("moksha",21,"tester","selenium");
   constructor() {
-    try {
-      this.service.addDeveloper("himanshu", 21, "java");
-      this.service.addDeveloper("imran", 22, "javascript");
-      this.service.addTester("moksha", 22, "selenium");
-      this.service.addTester("aditi", 22, "selenium");
-
-      this.foundDeveloper = this.service.findDeveloperByUsername("aaa");
-      console.log("found developer", this.foundDeveloper)
-      this.foundTester = this.service.findTesterByUsername("moksha");
-      console.log("found tester", this.foundTester);
-    } catch (error) {
-      console.log("error details ", error);
-      this.error = error;
-    }
+   
 
   }
 
