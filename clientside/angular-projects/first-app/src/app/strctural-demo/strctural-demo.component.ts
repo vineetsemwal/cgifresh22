b43382f47f23e2ee78391d6bca0ff040db2306bd
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-25 12:04:05 +0530

    *ngfor demo

--- clientside/angular-projects/first-app/src/app/strctural-demo/strctural-demo.component.ts
+++ clientside/angular-projects/first-app/src/app/strctural-demo/strctural-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Developer } from '../employee';
 
 @Component({
   selector: 'structural-demo',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class StructuralDemoComponent  {
 
 age:number=80;
+devs:Developer[]=[
+
+  new Developer("taran",21,"dev", "java"),
+  new Developer("deepak",22,"dev", "c#"),
+  new Developer("pedireddy",23,"dev", "python"),
+
+
+]
+
 
 isEligible():boolean{
 const flag=this.age>=18 && this.age<=60;
