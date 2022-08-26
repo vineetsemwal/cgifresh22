5f12d295000ca2541bd5bcf3dfb404535a82b903
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-26 12:22:09 +0530

    formhandling + validation

--- clientside/angular-projects/first-app/src/app/add-developer-template/add-developer-template.component.ts
+++ clientside/angular-projects/first-app/src/app/add-developer-template/add-developer-template.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'add-developer-template',
   templateUrl: './add-developer-template.component.html',
-  styleUrls: ['./add-developer-template.component.css']
+  styleUrls: ['./add-developer-template.component.css'],
 })
-export class AddDeveloperTemplateComponent  {
+export class AddDeveloperTemplateComponent {
+  username: string | undefined;
+  age: number | undefined;
+  language: string | undefined;
+  submitted: boolean = false;
 
-  username:string|undefined;
-  age:number|undefined;
-  language:string|undefined;
-  submitted:boolean=false;
+  isUsernameNotValid(usernameCtrl: any): boolean {
+    const flag =
+      (usernameCtrl.touched || usernameCtrl.dirty) &&
+      usernameCtrl.errors != null &&
+      usernameCtrl.errors['required'];
+    return flag;
+  }
 
-  addDeveloper(myform:any):void{
-    console.log("*****inside addDeveloper", myform);
+  addDeveloper(myform: any): void {
+    if (!myform.valid) {
+      return;
+    }
+    console.log('*****inside addDeveloper', myform);
     /*
     const formData =myform.value;
     this.username=formData.username;
     this.age=formData.age;
     this.language=formData.language;
     */
-    this.submitted=true;
-
-
+    this.submitted = true;
   }
 }
