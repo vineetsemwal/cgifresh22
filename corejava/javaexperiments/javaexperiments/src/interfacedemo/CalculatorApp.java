c6286969ba73d64cb58411e0cc7b4c8ee60cd0b0
vineetsemwal <vineetsemwal82@gmail.com>
2022-09-02 16:46:05 +0530

    corejava assignment demo added

---
+++ corejava/javaexperiments/javaexperiments/src/interfacedemo/CalculatorApp.java
@@ -0,0 +1,11 @@
+package interfacedemo;
+
+public class CalculatorApp {
+
+
+    public static void main(String args[]){
+        ICalculator calcy=new SamsungCalculator();
+        int result=calcy.add(1,2);
+        System.out.println("result="+result);
+    }
+}
