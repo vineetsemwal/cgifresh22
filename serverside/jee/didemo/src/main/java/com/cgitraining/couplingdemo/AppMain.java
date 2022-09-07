d91b1a077220ccf6c4e1dafd704edc3cbf9122cb
vineetsemwal <vineetsemwal82@gmail.com>
2022-09-07 11:14:20 +0530

    bean by nameused

--- serverside/jee/didemo/src/main/java/com/cgitraining/couplingdemo/AppMain.java
+++ serverside/jee/didemo/src/main/java/com/cgitraining/couplingdemo/AppMain.java
@@ -10,10 +10,15 @@ public class AppMain {
     public static void main(String args[]){
 
         ApplicationContext context=new AnnotationConfigApplicationContext(JavaConfig.class);
-        Canvas canvas=context.getBean(Canvas.class);
-        canvas.drawShape();
+        Canvas canvas1=context.getBean(Canvas.class);// fetched bean by type from container
+        canvas1.drawShape();
 
-        Square square=context.getBean(Square.class);
+        Square square=context.getBean(Square.class);// fetched bean by type from container
         System.out.println("side of square="+square.getSide());
+
+        Canvas canvas2= context.getBean("canvas",Canvas.class);//fetch bean by name
+        canvas2.drawShape();
+
+
     }
 }
